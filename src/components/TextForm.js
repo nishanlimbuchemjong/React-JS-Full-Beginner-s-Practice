1ad74577866d978facfdb09724a3795b75f0c00b
nishanlimbuchemjong <nishanlimbu.chemjong2000@gmail.com>
2024-12-08 23:43:43 +0545

    total word, character, minutes to read added and navbar color changed to dark

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -20,13 +20,19 @@ export default function TextForm(props) {
     }
   return (
     <>
-    <div className="mb-3">
+    <div className="container mb-3">
         <h1>{props.heading}</h1>
         <textarea className="form-control" value={text} onChange={handleOnChange} id="exampleFormControlTextarea1" rows="8"></textarea><br/>
-        <button className="btn btn-outline-primary" onClick={handleUpperCase}>Convert to UpperCase</button>
-        <button className="btn btn-outline-primary" onClick={handleLowerCase}>Convert to LowerCase</button>
+        <button className="btn btn-outline-primary mx-2" onClick={handleUpperCase}>Convert to UpperCase</button>
+        <button className="btn btn-outline-primary mx-2" onClick={handleLowerCase}>Convert to LowerCase</button>
     </div>
-    </>
+    <div className="container my-3">
+        <h1>Text Summary</h1>
+        <p>Total Word : {text.split(" ").length}</p>
+        <p>Total Character : {text.length}</p>
+        <p>Total Minutes to Read : {0.008*text.split(" ").length} minutes</p>
+    </div>
+    </> 
     
   )
 }
