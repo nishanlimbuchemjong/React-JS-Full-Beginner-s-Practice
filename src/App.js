86b9c225c805eb75534e90138429f1ac8992dcf8
nishanlimbuchemjong <nishanlimbu.chemjong2000@gmail.com>
2024-12-08 22:33:53 +0545

    TextUtils project setup and navbar with bootstrap5 added on the project

--- src/App.js
+++ src/App.js
@@ -1,19 +1,31 @@
 
 import './App.css';
 
-let name='Nishan Limbuy'
 function App() {
   return (
     <>
-    <nav>
-      <li>Home</li>
-      <li>About</li>
-      <li>Contact</li>
-    </nav><br />
-    <h1>Welcome to {name} React Practice</h1>
-    <div className="container">
-      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Explicabo, nostrum, voluptatem, in deleniti maiores eos alias rem aspernatur impedit tenetur unde sed! Blanditiis optio, architecto deleniti provident nobis minus velit.</p>
-    </div>
+    <nav className="navbar navbar-expand-lg bg-body-tertiary">
+      <div className="container-fluid">
+        <a className="navbar-brand" href="/">TextUtils</a>
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <a className="nav-link active" aria-current="page" href="/">Home</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="/">About</a>
+            </li>
+          </ul>
+          <form className="d-flex" role="search">
+            <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+            <button className="btn btn-outline-primary" type="submit">Search</button>
+          </form>
+        </div>
+      </div>
+    </nav>
 
     </>
     
