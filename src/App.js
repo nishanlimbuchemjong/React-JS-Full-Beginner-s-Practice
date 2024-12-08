6ed2969bb0fa8cbee315d38ad42d78bc113eba23
nishanlimbuchemjong <nishanlimbu.chemjong2000@gmail.com>
2024-12-08 20:51:03 +0545

    [update] jsx practiced

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
+
 import './App.css';
 
+let name='Nishan Limbuy'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React with Nishan Limbu
-        </a>
-      </header>
+    <>
+    <nav>
+      <li>Home</li>
+      <li>About</li>
+      <li>Contact</li>
+    </nav><br />
+    <h1>Welcome to {name} React Practice</h1>
+    <div className="container">
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Explicabo, nostrum, voluptatem, in deleniti maiores eos alias rem aspernatur impedit tenetur unde sed! Blanditiis optio, architecto deleniti provident nobis minus velit.</p>
     </div>
+
+    </>
+    
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -6,7 +6,9 @@ import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <h1>Hello World</h1>
+  <React.StrictMode>
+    <App/>
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
