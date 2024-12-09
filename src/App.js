8470c837d7667b7dbdf56a26c759d808a2ce4862
nishanlimbuchemjong <nishanlimbu.chemjong2000@gmail.com>
2024-12-09 20:15:14 +0545

    Alert.js file created. alert message showned when dark switch mode is on and light mode is on

--- src/App.js
+++ src/App.js
@@ -3,24 +3,35 @@ import './App.css';
 import About from './components/About';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
+import Alert from './components/Alert';
 
 function App() {
   const [mode, setMode] = useState('light');
+  const [alert, setAlert] = useState(null);
 
+  const showAlert = (message, type)=>{
+      setAlert({
+        msg: message,
+        type: type
+      })
+  }
   const toggleMode = ()=>{
     if(mode === 'light'){
       setMode('dark');
       document.body.style.backgroundColor = 'grey';
+      showAlert("Dark Mode has been enabled", "success");
     } else {
       setMode('light');
       document.body.style.backgroundColor = 'white';
+      showAlert("Light Mode has been enabled", "success");
     }
   }
   return (
     <>
     <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode}/>
+    <Alert alert={alert}/>
     <div className="container">
-    <TextForm heading="Enter the text to Analyze" mode={mode}/>
+    <TextForm heading="Enter the text to Analyze" mode={mode} />
     {/* <About/> */}
     </div>
     </>
