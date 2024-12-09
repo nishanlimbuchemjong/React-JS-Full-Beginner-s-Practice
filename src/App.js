d1f0178a969aa0064b667bdeaa38602c17d65cbe
nishanlimbuchemjong <nishanlimbu.chemjong2000@gmail.com>
2024-12-09 19:34:21 +0545

    dark and light switch mode added on navbar

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
-
+import React, { useState } from 'react';
 import './App.css';
 import About from './components/About';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
 
 function App() {
+  const [mode, setMode] = useState('dark');
+
+  const toggleMode = ()=>{
+    if(mode === 'light'){
+      setMode('dark');
+    } else {
+      setMode('light');
+    }
+  }
   return (
     <>
-    <Navbar title="TextUtils"/>
+    <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode}/>
     <div className="container">
-    {/* <TextForm heading="Enter the text to Analyze"/> */}
-    <About/>
+    <TextForm heading="Enter the text to Analyze"/>
+    {/* <About/> */}
     </div>
     </>
     
