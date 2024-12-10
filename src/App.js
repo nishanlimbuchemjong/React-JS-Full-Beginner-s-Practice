af8773ba1e383f41a880219bb2c2951a62110b1a
nishanlimbuchemjong <nishanlimbu.chemjong2000@gmail.com>
2024-12-10 22:34:50 +0545

    navbar for newsapp added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import './App.css';
 
 import React, { Component } from 'react'
+import NavBar from './components/NavBar';
 
 export default class App extends Component {
   render() {
     return (
       <div>
-        hello my first class based component
+        <NavBar/>
       </div>
     )
   }
