1b96b4b47a43963e0b7f0d759cc0704b138499aa
nishanlimbuchemjong <nishanlimbu.chemjong2000@gmail.com>
2024-12-10 22:39:50 +0545

    News component added

---
+++ src/components/News.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class news extends Component {
+  render() {
+    return (
+      <div>
+        This is a news component
+      </div>
+    )
+  }
+}
+
+export default news
